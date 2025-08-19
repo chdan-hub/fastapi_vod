e807512b0bebcd75cd5b950498f5ca39f9243c04
chdan-hub <ksmii358@gmail.com>
2025-08-18 18:28:02 -0700

    install mypy

--- temp.py
+++
@@ -1,8 +0,0 @@
-my_int: int = 123 # Literal: 값
-my_str: str = "abc"
-
-my_list: list[str] = ["abc", "def"]
-
-my_tuple: tuple[str, str] = ("abc", "def")
-
-my_dict: dict[str, int] = {"a": 1, "b": 2}

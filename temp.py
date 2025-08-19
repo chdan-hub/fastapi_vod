145712014811a4c8b1453ae72d5de13f5ee2aa45
chdan-hub <ksmii358@gmail.com>
2025-08-18 18:20:13 -0700

    1일차 수정 및 정리

---
+++ temp.py
@@ -0,0 +1,8 @@
+my_int: int = 123 # Literal: 값
+my_str: str = "abc"
+
+my_list: list[str] = ["abc", "def"]
+
+my_tuple: tuple[str, str] = ("abc", "def")
+
+my_dict: dict[str, int] = {"a": 1, "b": 2}

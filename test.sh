8ba1755120be2bf8589697e43bace0c2145b388b
chdan-hub <ksmii358@gmail.com>
2025-08-18 23:23:36 -0700

    tete commit three

--- test.sh
+++ test.sh
@@ -1,27 +1,26 @@
 set -eo pipefail
 
 COLOR_GREEN=`tput setaf 2;`
-COLOR_NC=`tput sgr0;` # no color
+COLOR_NC=`tput sgr0;` # No Color
 
-echo  "Starting black"
+echo "Starting black"
 poetry run black .
-echo  "OK"
+echo "OK"
 
-echo  "Starting ruff"
-poetry  run ruff check --select I --fix
-poetry  run ruff check --fix
-echo  "OK"
+echo "Starting ruff"
+poetry run ruff check --select I --fix
+poetry run ruff check --fix
+echo "OK"
 
-echo  "Starting mypy"
+echo "Starting mypy"
 poetry run mypy .
-echo  "OK"
+echo "OK"
 
 echo "Starting pytest with coverage"
 poetry run coverage run -m pytest
 poetry run coverage report -m
 poetry run coverage html
-echo  "OK"
 
-echo  "${COLOR_GREEN}All tests passed successfully!${COLOR_NC}"
+echo "${COLOR_GREEN}All tests passed successfully!${COLOR_NC}"
 
 

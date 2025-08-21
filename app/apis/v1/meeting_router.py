603f03fabe22c71aa3d66a93fbd627c317ceff05
chdan-hub <ksmii358@gmail.com>
2025-08-20 22:36:13 -0700

    meeting 생성 기능 구현 완료

--- app/apis/v1/meeting_router.py
+++ app/apis/v1/meeting_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.dtos.create_meeting_response import CreateMeetingResponse
+from app.service.meeting_service_mysql import service_create_meeting_mysql
 
 edgedb_router = APIRouter(prefix="/v1/edgedb/meetings", tags=["Meeting"], redirect_slashes=False)
 mysql_router = APIRouter(prefix="/v1/mysql/meetings", tags=["Meeting"], redirect_slashes=False)
@@ -19,4 +20,4 @@ async def api_create_meeting_edgedb() -> CreateMeetingResponse:
     description="meeting 을 생성합니다.",
 )
 async def api_create_meeting_mysql() -> CreateMeetingResponse:
-    return CreateMeetingResponse(url_code="abc")
+    return CreateMeetingResponse(url_code=(await service_create_meeting_mysql()).url_code)

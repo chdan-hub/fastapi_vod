6b632847af7250bfce144d50a225785b780f2427
chdan-hub <ksmii358@gmail.com>
2025-08-21 18:53:59 -0700

    add get meeting apec

--- app/apis/v1/meeting_router.py
+++ app/apis/v1/meeting_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.dtos.create_meeting_response import CreateMeetingResponse
+from app.dtos.get_meetng_response import GetMeetingResponse
 from app.service.meeting_service_mysql import service_create_meeting_mysql
 
 edgedb_router = APIRouter(prefix="/v1/edgedb/meetings", tags=["Meeting"], redirect_slashes=False)
@@ -21,3 +22,19 @@ async def api_create_meeting_edgedb() -> CreateMeetingResponse:
 )
 async def api_create_meeting_mysql() -> CreateMeetingResponse:
     return CreateMeetingResponse(url_code=(await service_create_meeting_mysql()).url_code)
+
+
+@edgedb_router.get(
+    "/{meeting_url_code}",
+    description="meeting 을 조회합니다.",
+)
+async def api_get_meeting_edgedb(meeting_url_code: str) -> GetMeetingResponse:
+    return GetMeetingResponse(url_code="abc")
+
+
+@mysql_router.get(
+    "/{meeting_url_code}",
+    description="meeting 을 조회합니다.",
+)
+async def api_get_meeting_mysql(meeting_url_code: str) -> GetMeetingResponse:
+    return GetMeetingResponse(url_code="abc")
--- temp.py
+++
@@ -1,6 +0,0 @@
-def add(a: int, b: int) -> int:
-
-    return a + b
-
-
-# 집가고싶다
